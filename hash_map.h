9f8b5db5f0564aaa55c84b9a6d4f4b3c6c98e7af
Marcus Lian Hanestad <marlhan@proton.me>
2024-11-30 12:26:03 +0100

    remove old unused hash map

--- hash_map.h
+++ hash_map.h
@@ -8,36 +8,6 @@
 #define HASH_MAP_DEFAULT_BUCKET_COUNT 512
 #define HASH_MAP_THRESHOLD 0.75
 
-// Is linked list okay?
-struct linked_list_node {
-  char *key;
-  char *value;
-  struct linked_list_node *next;
-};
-
-struct hash_map_bucket {
-  struct linked_list_node *root;
-};
-
-struct hash_map {
-  uint32_t (*hash)(const void * value);
-  bool (*compare)(const void *rhs, const void *lhs);
-  struct hash_map_bucket *buckets;
-  size_t entries;
-  size_t n_buckets;
-};
-
-struct hash_map hash_map_new(uint32_t (*hash)(const void *key), bool (*compare)(const void *lhs, const void *rhs));
-struct hash_map hash_map_new_with_cap(size_t cap, uint32_t (*hash)(const void *key), bool (*compare)(const void *lhs, const void *rhs));
-void hash_map_insert(struct hash_map *self, void *key, void *value, size_t key_size, size_t value_size);
-void *hash_map_get(struct hash_map *self, void *key);
-void *hash_map_get_or_insert(struct hash_map *self, void *key, void *value, size_t key_size, size_t value_size);
-bool hash_map_contains(struct hash_map *self, void *key);
-void hash_map_free(struct hash_map *self);
-
-uint32_t hash_map_hash_char_star(const void *key);
-bool hash_map_compare_char_star(const void *lhs, const void *rhs);
-
 // Hash map for key:char* and value:float
 struct linked_list_node_cf {
   char *key;
--- test.c
+++ test.c
@@ -10,63 +10,6 @@
 #define test(testfn) testfn();                  \
     printf("\033[32m[+]\033[0m %s \033[32mpassed\033[0m\n", #testfn);
 
-void hash_map_print_char_star_int(struct hash_map *map) {
-  printf("Entries in map: %li\n", map->entries);
-  for (size_t i = 0; i < map->n_buckets; i++) {
-    struct linked_list_node *node = map->buckets[i].root;
-    while (node) {
-      printf("\t(bucket) %li : (key) %s : (value) %i\n", i, node->key, *(int *)node->value);
-      node = node->next;
-    }
-  }
-}
-
-void test_hash_map_basic_insert_get() {
-  struct hash_map map = hash_map_new_with_cap(3, hash_map_hash_char_star, hash_map_compare_char_star);
-  int a=1, b=2, c=3, d=4, e=5;
-  hash_map_insert(&map, "key1", &a, 5, sizeof(int));
-  hash_map_insert(&map, "key2", &b, 5, sizeof(int));
-  hash_map_insert(&map, "key3", &c, 5, sizeof(int));
-  hash_map_insert(&map, "key4", &d, 5, sizeof(int));
-  hash_map_insert(&map, "key5", &e, 5, sizeof(int));
-
-  assert(map.entries == 5);
-
-  assert(hash_map_contains(&map, "key1"));
-  assert(hash_map_contains(&map, "key2"));
-  assert(hash_map_contains(&map, "key3"));
-  assert(hash_map_contains(&map, "key4"));
-  assert(hash_map_contains(&map, "key5"));
-
-  assert(*(int *)hash_map_get(&map, "key1") == a);
-  assert(*(int *)hash_map_get(&map, "key2") == b);
-  assert(*(int *)hash_map_get(&map, "key3") == c);
-  assert(*(int *)hash_map_get(&map, "key4") == d);
-  assert(*(int *)hash_map_get(&map, "key5") == e);
-
-  hash_map_free(&map);
-}
-
-void test_hash_map_basic_insert_get_big() {
-  struct hash_map map = hash_map_new(hash_map_hash_char_star, hash_map_compare_char_star);
-
-  char str[7];
-  size_t upper = 100000;
-  for (size_t i = 0; i < upper; i++) {
-    sprintf(str, "%li", i);
-    int val = upper - i;
-    hash_map_insert(&map, str, &val, strlen(str) + 1, sizeof(int));
-  }
-
-  assert(map.entries == upper);
-
-  for (size_t i = 0; i < upper; i++) {
-    sprintf(str, "%li", i);
-    assert(*(int *)hash_map_get(&map, str) == (int)(upper - i));
-  }
-  hash_map_free(&map);
-}
-
 void test_hash_map_cf_insert_get() {
   struct hash_map_cf map = hash_map_cf_new_with_cap(3);
   float a=1.1, b=2.2, c=3.3, d=4.4, e=5.5;
@@ -114,9 +57,7 @@ void test_hash_map_cf_insert_get_big() {
 }
 
 int main() {
-  test(test_hash_map_basic_insert_get);
   test(test_hash_map_cf_insert_get);
-  test(test_hash_map_basic_insert_get_big);
   test(test_hash_map_cf_insert_get_big);
   return 0;
 }
